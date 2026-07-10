5a93c335baf7be70cdcf6ec068e6f2578d668fcc
vantablade <maksimhabarovs388@gmail.com>
2026-07-10 21:56:39 +0800

    Added orientation and corner identification

--- Python Files/analysis.py
+++ Python Files/analysis.py
@@ -2,18 +2,17 @@ from cube import create_solved_cube
 cube = create_solved_cube()
 
 def count_face_colours(cube, face):
-    for face in cube:
-        count = {}
-        for sticker in cube[face]:
-            if sticker in count:
-                count[sticker] += 1
-            else:
-                count[sticker] = 1
-        return count
+    count = {}
+    for sticker in cube[face]:
+        if sticker in count:
+            count[sticker] += 1
+        else:
+            count[sticker] = 1
+    return count
 
 def most_completed_face(cube):
     highest_face = None
-    highest_count = 0
+    best_count = 0
     best_colour = None
     
     for face in cube:
@@ -25,12 +24,29 @@ def most_completed_face(cube):
         face_best_colour = max(counts, key=counts.get)
         face_max_count = counts[face_best_colour]
         
-        if face_max_count > highest_count:
-            highest_count = face_max_count
+        if face_max_count > best_count:
+            best_count = face_max_count
             highest_face = face
             best_colour = face_best_colour
             
-    return highest_face, highest_count, best_colour
+    return highest_face, best_count, best_colour
 
 face, count, colour = most_completed_face(cube)
 print(f"Face: {face}, Count: {count}, Colour: {colour}")
+
+def get_face(cube, orientation, face):
+    return cube[orientation[face]]
+
+orientation = {
+    "U": "U",
+    "D": "D",
+    "F": "F",
+    "B": "B",
+    "L": "L",
+    "R": "R",
+}
+
+CORNERS = {
+    "UFR": ()
+}
+        
--- Python Files/main.py
+++ Python Files/main.py
@@ -1,8 +1,9 @@
 from cube import create_solved_cube
 from display import display_cube
 from moves import *
+from analysis import *
 
 cube = create_solved_cube()
 
-cube = apply_algorithm(cube, "R U R' U'")
-display_cube(cube)
+cube = most_completed_face(cube)
+print(most_completed_face)
